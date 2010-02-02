605810c1232dda187a0937b510200fe503496cff
Tony Angerilli <tony@angerilli.ca>
2010-02-01 22:24:09 -0800

    Initialized repo with the current state of the Spark Integration tmk fork

---
+++ modules/revision.template.py
@@ -0,0 +1,25 @@
+###################################
+#   AUTOMATICALLY GENERATED FILE
+###################################
+
+# Revision of the local directory
+REVISION    =   "{REVISION}"
+DIRECTORY   =   "{DIRECTORY}"
+URL         =   "{URL}"
+
+# Date of generation
+GENERATION_DATE     =   "{GEN_DATE}"
+
+# Name of the machine file was generated on
+GENERATION_MACHINE  =   "{BUILD_MACHINE}"
+
+# Convenient formatted string
+REVISION_INFO       =   "=== Revision Information ===\n"    \
+                        "Revision:        %s\n"             \
+                        "Directory:       %s\n"             \
+                        "URL:             %s\n"             \
+                        "Generation Date: %s\n"             \
+                        "Generator:       %s\n"             \
+                        "======\n" %                        \
+                        (REVISION, DIRECTORY, URL,
+                        GENERATION_DATE, GENERATION_MACHINE)
